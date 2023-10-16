cfa2af891191881f72be0392acbbc6bc334b0c08
Natalia <nataliazanella@yahoo.com.br>
2023-10-16 17:07:06 -0300

    projeto-meubanco

---
+++ ContaBanco/src/Conta.java
@@ -0,0 +1,16 @@
+
+public class Conta {
+
+    public String getNome() {
+        return null;
+    }
+
+    public String getNumeroConta() {
+        return null;
+    }
+
+    public String getSaldo() {
+        return null;
+    }
+
+}
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitar informações ao usuário
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = Double.parseDouble(scanner.nextLine());
+        
+        // Fechar o Scanner
+        scanner.close();
+        
+        // Exibir mensagem formatada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
+
+
+
+
+
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem de conta criada
+    
+
+
+
